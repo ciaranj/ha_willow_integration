4366d926bf4295f208894376c3ece258142afee3
ciaranj <ciaranj@gmail.com>
2025-04-17 21:50:55 +0000

    use the willow events to set the entity state

--- assist_satellite.py
+++ assist_satellite.py
@@ -6,9 +6,11 @@ import logging
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import device_registry as dr
 from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
+from homeassistant.components.assist_satellite.entity import AssistSatelliteState
 
 from .const import DOMAIN
 
@@ -24,7 +26,7 @@ async def async_setup_entry(
 
     async_add_entities(
         [
-            Willow(device["label"], device["mac_addr"])
+            Willow(hass, device["label"], device["mac_addr"])
             for device in config_entry.runtime_data
         ]
     )
@@ -33,10 +35,27 @@ async def async_setup_entry(
 class Willow(Entity):
     """Representation of a Willow Unit."""
 
-    def __init__(self, name, mac) -> None:
+    def __init__(self, hass, name, mac) -> None:
         """Initialize an a Willow entity."""
         self._name = name
-        self._attr_unique_id = mac
+        self._attr_unique_id = f"{mac}-satellite"
+        self._registry = dr.async_get(hass)
+        self._deviceidentifier = (DOMAIN, mac)
+        self._attr_state = AssistSatelliteState.IDLE
+
+        hass.bus.async_listen("willow_event", self._handle_event)
+
+    def _handle_event(self, call):
+        # TODO: could probably cache the device id for this entity
+        device = self._registry.async_get(call.data["device_id"])
+        if self._deviceidentifier in device.identifiers:
+            if call.data["type"] == "wake_start":
+                self._attr_state = AssistSatelliteState.LISTENING
+            elif call.data["type"] == "wake_end":
+                self._attr_state = AssistSatelliteState.PROCESSING
+            elif call.data["type"] == "command_finished":
+                self._attr_state = AssistSatelliteState.IDLE
+            self.schedule_update_ha_state(True)
 
     @property
     def name(self) -> str:
@@ -46,4 +65,4 @@ class Willow(Entity):
     @property
     def device_info(self) -> DeviceInfo:
         """Return the device info."""
-        return DeviceInfo(identifiers={(DOMAIN, self._attr_unique_id)}, name=self._name)
+        return DeviceInfo(identifiers={self._deviceidentifier}, name=self._name)
